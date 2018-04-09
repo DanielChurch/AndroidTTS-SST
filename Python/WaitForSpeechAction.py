8499ca34e40a3fdf3223cba3331e18c8d48d414a
Dieter103 <grosswiler2@gmail.com>
2018-04-08 21:36:13 -0600

    a

--- Python/WaitForSpeechAction.py
+++ Python/WaitForSpeechAction.py
@@ -10,17 +10,12 @@ class WaitForSpeechAction(Action):
 
         # Properties in format (GuiType, initialValue, min, max)
 
-<<<<<<< HEAD
+
     def move_robot(self, args, controller):
         action = MotorAction()
         action.run_custom(controller, args[1], args[2])
 
-=======
-    def move_robot(self, args):
-        # args is a list of arguments
-        # eg move 5
-        #                      5
->>>>>>> 7c7c35fba798fa4382498b391228acb3a65b4a80
+
         print("Moving Robot", args[0])
 
     def turn_robot(self, args):
@@ -30,12 +25,10 @@ class WaitForSpeechAction(Action):
         # or
         (direction, amount) = args
         print("Turning Robot", direction, amount)
-<<<<<<< HEAD
+
     def run_now(self):
         print("running")
         return
-=======
->>>>>>> 7c7c35fba798fa4382498b391228acb3a65b4a80
 
     # Override run function
     def run(self, controller, server):
@@ -46,16 +39,14 @@ class WaitForSpeechAction(Action):
 
         command = server.clients[0].command
         args = server.clients[0].args
-<<<<<<< HEAD
+
         print(server.clients[0].command)
         if (server.clients[0].command == 'move'):
             self.move_robot(controller, args)
             print("in to move func")
         else:
             pass
-=======
 
->>>>>>> 7c7c35fba798fa4382498b391228acb3a65b4a80
         server.clients[0].reset()
 
         {
@@ -63,14 +54,7 @@ class WaitForSpeechAction(Action):
             'turn': self.turn_robot,  # eg. turn left 5
             'exit': lambda e: exit(),
         }.get(command, lambda e: print('Invalid command'))(args)
-<<<<<<< HEAD
 
-        # args = server.run()
-
-
-        #     return
-=======
->>>>>>> 7c7c35fba798fa4382498b391228acb3a65b4a80
 
     def copy(self):
         return WaitForSpeechAction()
